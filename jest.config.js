6ca8e4b7e4135e5f616cc37a62181270ab993690
Cole Schneider <colekschneider@gmail.com>
2019-07-13 11:48:34 -0700

    circle and test config

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,65 @@
 module.exports = {
     cacheDirectory: "<rootDir>/jestCache",
-    verbose: false,
-    collectCoverageFrom: ["src/**/*.{js,jsx,mjs,ts,tsx}"],
-    setupFiles: [
-        "react-app-polyfill/jsdom",
-        "<rootDir>/node_modules/regenerator-runtime/runtime"
+    collectCoverageFrom: [
+        "**/*.{ts,tsx}",
+        "!**/node_modules/**",
+        "!**/*.d.ts",
+        "!**/{test,tests}/**"
     ],
-    setupFilesAfterEnv: ["<rootDir>config/jest/setup.js"],
-    testMatch: ["<rootDir>/src/**/*.(spec|test).{js,jsx,mjs,ts,tsx}"],
+    coveragePathIgnorePatterns: [
+        "/types/",
+        "/src/images",
+        "/src/App.tsx",
+        "/src/Routes.tsx",
+        "/src/index.tsx",
+        "/src/serviceWorker.ts",
+        "src/Common/LazyRoute/LazyRoute.tsx"
+    ],
+    coverageThreshold: {
+        global: {
+            branches: -53,
+            functions: -22,
+            lines: -67,
+            statements: -75
+        }
+    },
+    modulePathIgnorePatterns: ["<rootDir>/build/"],
+    coverageReporters: ["json-summary", "text", "lcov"],
+    resolver: "jest-pnp-resolver",
+    setupFiles: ["react-app-polyfill/jsdom", "<rootDir>/config/jest/setup.js"],
+    snapshotSerializers: ["enzyme-to-json/serializer"],
+    setupFilesAfterEnv: ["<rootDir>/config/jest/setup.js"],
+    testMatch: [
+        "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+        "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}"
+    ],
+    verbose: false,
     testEnvironment: "jsdom",
     testURL: "http://localhost",
     transform: {
-        "^.+\\.(js|jsx|mjs|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
         "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
-        "^(?!.*\\.(js|jsx|mjs|ts|tsx|css|json)$)":
+        "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)":
             "<rootDir>/config/jest/fileTransform.js"
     },
-    moduleNameMapper: {
-        "^react-native$": "react-native-web",
-        "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
-    },
     transformIgnorePatterns: [
-        "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx|mjs)$"
+        "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$",
+        "^.+\\.module\\.(css|sass|scss)$"
     ],
-    moduleFileExtensions: ["js", "json", "jsx", "node", "mjs", "ts", "tsx"],
+    moduleFileExtensions: [
+        "web.js",
+        "js",
+        "web.ts",
+        "ts",
+        "web.tsx",
+        "tsx",
+        "json",
+        "web.jsx",
+        "jsx",
+        "node"
+    ],
     watchPlugins: [
-        "jest-watch-typeahead/filename",
-        "jest-watch-typeahead/testname"
+        "<rootDir>/node_modules/jest-watch-typeahead/filename.js",
+        "<rootDir>/node_modules/jest-watch-typeahead/testname.js"
     ]
 };
--- src/sections/Header/Header.spec.tsx
+++ src/sections/Header/Header.spec.tsx
@@ -3,16 +3,8 @@ import Header from "./Header";
 
 describe("Header", () => {
     let wrapper;
-    const windowSpy = jest.fn();
-    beforeAll(() => {
-        window.addEventListener("resize", windowSpy);
-    });
-
     it("should match the snapshot", () => {
-        // @ts-ignore
-        window.innerWidth = 320;
-        window.dispatchEvent(new Event("resize"));
         wrapper = shallow(<Header />);
-        console.log(wrapper.debug(), windowSpy, window.innerWidth);
+        expect(<Header />).toMatchSnapshot();
     });
 });
